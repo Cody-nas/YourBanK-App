39b96b69bfbcf0f3c0be6374682b8ea1a042a156
Cody-nas <nasiruadamu214@gmail.com>
2025-01-11 08:54:19 +0100

    Update components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import Hero from '../components/Hero';
+import Products from '../components/Products'
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Products />
     </div>
   );
 };
