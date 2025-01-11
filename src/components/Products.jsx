7ccba0a1e9f98be8db8e1faae0a769ac228051d4
Cody-nas <nasiruadamu214@gmail.com>
2025-01-11 18:50:32 +0100

    update products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>
+    <div className='md:px-12 p-4 max-w-screen-2xl mx-auto mt-24'>
+      <div className='flex flex-col md:flex-row justify-evenly items-center gap-6'>
+        <h2 className='text-4xl '>Our <span className='text-grn'>Products</span></h2>
 
+      </div>
     </div>
   )
 }
