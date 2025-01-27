f9695e610d8250d9d808db0e12b3186f9dd30d7f
Cody-nas <nasiruadamu214@gmail.com>
2025-01-27 17:16:58 +0100

    Add FAQ component to Security page for improved user support

--- src/Pages/Security.jsx
+++ src/Pages/Security.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import SecurityHero from '../components/SecurityHero';
 import Protect from '../components/Protect';
+import Faq from '../components/FAQ'
 
 const Security = () => {
   return (
     <div>
       <SecurityHero />
       <Protect />
+      <Faq />
     </div>
   )
 }
